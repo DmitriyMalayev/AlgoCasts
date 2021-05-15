886804c54ec788d77333512670ad808b2be0422d
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-05-15 16:43:19 -0400

    solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,39 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str) {   //Declaring a variable called character. str is the iterable object that we want to loop through. 
+//     reversed = character + reversed;
+
+//   }
+//   return reversed;
+// }
+
+//""  =  "a" + ""    "a"
+
+//"a"  =  "p" + "a"     "pa"
+//"pa"  =  "p" + "pa"     "ppa"
+//"ppa" = "l" + "ppa"  "lppa"
+//"lppa" = "e" + "lppa" "ellpa"
+
+//elppa
+//apple   
+
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "")
+}
+
+reverse("asdf")
 
 module.exports = reverse;
+
+
+
+
+// .reduce takes two arguments, an arrow function and a starting value. 
