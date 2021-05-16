5cbd9357f09c0f3387584cdd94083928942e4282
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-05-15 21:54:08 -0400

    started

--- completed_exercises/reverseint/index.js
+++ completed_exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+
+
+
+
+//Result should be a number not a string
+const myNumber = 200
+myNumber.toString().split("").join("") //returns a string version of the number  "200"
+
+
+
+
+
+
+
+
+
+
+
 function reverseInt(n) {
   const reversed = n
     .toString()
